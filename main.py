ab7f4d5f21a41b38db1ac880ceb68e846f17d92e
g8t1b <g8t1b@ugrad.cs.ubc.ca>
2019-03-12 12:59:45 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,35 @@
-from model.TupledKeyDict import TupledKeyDict
+#! /usr/bin/env python
 
 __author__ = "awakenedhaki"
 
+import os
+import json
 import click
 
+from model.Problem import Problem
+from model.TupledKeyDict import TupledKeyDict
 from persistence.ProblemsJSON import ProblemsJSON
 
+with open("config.json", "r") as infile:
+    config = json.load(infile)
+
+home = os.getenv("HOME")
+path = os.path.join(home, config["path"])
+
 @click.command()
-@click.option("-n", "--next")
-@click.option("-s", "--select")
-@click.option("-c", "--check")
-@click.option("-l", "--list", is_flag=True)
-def main(next, select, check, list):
-    problems = ProblemsJSON().unferment()
-    pass
+@click.option("-p", "--problem", help="Problem ID or index")
+@click.option("-l", "--language", default="python",
+              help="Programming language for template")
+@click.option("--listed", is_flag=True)
+@click.option("-v", "--verbose", is_flag=True)
+def main(problem, language, listed, verbose):
+    problems: TupledKeyDict = ProblemsJSON().unferment()
+    if problem:
+        Problem().setUp(problems[problem],
+                        path,
+                        config["templates"][language])
+    if listed:
+        pass
 
 if __name__ == '__main__':
     main()
