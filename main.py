8b7cb28f9405f848aa2b5a0b8ad1c0143bc4251c
g8t1b <g8t1b@ugrad.cs.ubc.ca>
2019-03-12 17:25:01 -0700

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import os
 import json
 import click
 
+from utils import printColumns
 from model.Problem import Problem
 from model.TupledKeyDict import TupledKeyDict
 from persistence.ProblemsJSON import ProblemsJSON
@@ -16,19 +17,25 @@ with open("config.json", "r") as infile:
 home = os.getenv("HOME")
 path = os.path.join(home, config["path"])
 
-@click.command()
+@click.command(help="Command-line interface for Rosalind Bioinformatics Stronghold Problems.")
 @click.option("-p", "--problem", help="Problem ID or index")
 @click.option("-l", "--language", default="python",
               help="Programming language for template")
-@click.option("--listed", is_flag=True)
+@click.option("--listids", is_flag=True)
+@click.option("--check", is_flag=True)
 @click.option("-v", "--verbose", is_flag=True)
-def main(problem, language, listed, verbose):
+def main(problem, language, listids, check, verbose):
     problems: TupledKeyDict = ProblemsJSON().unferment()
     if problem:
-        Problem().setUp(problems[problem],
+        Problem().setUp(problems[problem.upper()],
                         path,
                         config["templates"][language])
-    if listed:
+    if listids:
+        keys = map(lambda e: " - ".join(e), problems.keys())
+        printColumns(keys)
+    if check:
+        pass
+    if verbose:
         pass
 
 if __name__ == '__main__':
